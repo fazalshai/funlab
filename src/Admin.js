423273c7aad27d6af0f86627bcf725c5dd9aaa5f
fazal shaik <102577015+fazalshai@users.noreply.github.com>
2025-10-22 15:22:24 +0530

    Update Admin.js

--- src/Admin.js
+++ src/Admin.js
@@ -35,7 +35,7 @@ export default function Admin() {
     30: { name: "Jyoshika", regNo: "REG030" },
       31: { name: "Daiwik ", regNo: "REG029" },
     32: { name: "Chakraborty", regNo: "REG030" },
-      33: { name: "Smitha", regNo: "REG029" },
+      33: { name: "Pavan varma", regNo: "REG029" },
     34: { name: "Jyoshika", regNo: "REG030" },
   };
 
