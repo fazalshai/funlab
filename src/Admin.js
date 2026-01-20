1e7dc4f2f980b5eb1f4f7c6c5ada9505ddcfd12d
ABDUL MUSAVVIR <abdulmusavvir2000@gmail.com>
2026-01-20 15:27:13 +0530

    Update Admin.js

--- src/Admin.js
+++ src/Admin.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Admin() {
-  const BASE_URL = "https://funlab-backend-ot1x.onrender.com"; 
+  const BASE_URL = "https://funlab-backend-1-xr53.onrender.com/log.php"; 
 
   // ✅ Manual ID-to-name mapping
   const idToNameMap = {
